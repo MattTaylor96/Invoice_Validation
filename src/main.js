c30b893845dff750cc1838a87f59193e347cd5ef
TatTheFlame <50228916+TatTheFlame@users.noreply.github.com>
2019-05-16 08:09:08 +0100

    Remove Defunct Code
    Remove defunct code and testing console lines.

--- src/main.js
+++ src/main.js
@@ -1,49 +1,55 @@
-var fileInput = document.getElementById("file-input");
-var customerInput = document.getElementById("customer-input");
-var invoicesProcessed;
-var customersProcessed;
-var customerID = [];
-var customerAddressID = [];
-var invoiceOutput = document.getElementById("invoice-output");
-var invoiceErrors = document.getElementById("errors");
+// Declare variables for use
+var fileInput = document.getElementById("file-input"); // Invoice file uploaded
+var customerInput = document.getElementById("customer-input"); // Customer file uploaded
+var invoicesProcessed; // Invoices in JSON format
+var customersProcessed; // Customers in JSON format
+var customerID = []; // Array to store customer IDs 
+var customerAddressID = []; // Array to store customer address IDs
+var invoiceOutput = document.getElementById("invoice-output"); // HTML element for displaying invoice lines
+var invoiceErrors = document.getElementById("errors"); // HTML element for displaying invoice errors
 
+// On customer file upload...
 function submitCustomers(){
-	// If no file is entered
+	// If no file is entered...
 	if(!customerInput.files[0]){
+		// Alert and return to stop errors
 		alert("No File Uploaded");
 		return
 	}
-	// Parse customer data
+	// Parse customer data (PapaParse)
 	Papa.parse(customerInput.files[0], {
+		// PapaParse config - callback for results
 		complete: function(results) {
+			// Store JSON in variable and allocate
 			customersProcessed = results;
 			allocateCustomers();
 			// Create new elements in the DOM to show files
 			let node = document.createElement("li");
 			node.innerHTML = customerInput.files[0].name; 
 			document.getElementById("customer-files").appendChild(node);
-			console.log(customerID);
-			console.log(customerAddressID);
 		}
 	});
 }
 
+// On invoice file upload...
 function submitFile(){
-	// If no file is entered
+	// If no file is entered...
 	if(!fileInput.files[0]){
+		// Alert and return to prevent errors
 		alert("No File Uploaded");
 		return
 	}
-	// If no customer file is entered
+	// If no customer file is entered...
 	if(!customerInput.files[0]){
+		// Alert and return to prevent errors
 		alert("Please upload a customer file first")
 		return
 	}
 	// Parse the data (return through a callback due to asynchronous parsing)
 	Papa.parse(fileInput.files[0], {
 		complete: function(results) {
+			// Output invoice lines and validate data
 			invoicesProcessed = results;
-			console.log(invoicesProcessed);
 			outputInvoices();
 			outputErrors();			
 			// Create new elements in the DOM to show files
@@ -54,46 +60,52 @@ function submitFile(){
 	});	
 }
 
+// Remove all invoice data from the page
 function clearPage(){
+	// Determine last children of elements
 	let x = document.getElementById("invoice-output");
 	let y = document.getElementById("errors");
 	let xChild = x.lastElementChild;
 	let yChild = y.lastElementChild;
-	// Loop and delete all child elements
+	// While there is a child element...
 	while(xChild){
 		x.removeChild(xChild);
+		// Set new element as last child
 		xChild = x.lastElementChild;
 	}
+	// While there is a child element...
 	while(yChild){
 		y.removeChild(yChild);
+		// Set new element as last child
 		yChild = y.lastElementChild;
 	}
 }
 
+// Toggle display on invoice lines
 function hideLines(){
 	invoiceOutput.classList.toggle("hidden");
 }
 
+
 function allocateCustomers(){
 	for(let i = 0; i < customersProcessed.data.length; i++){
-		// Move Customer reference into array if it doesn't exist
+		// Move Customer reference into array if it doesn't already exist
 		if(customerID.indexOf(customersProcessed.data[i][1]) < 0){
 			customerID.push(customersProcessed.data[i][1]);;
 		}
-		// Move Office reference into array if it doesn't exist
+		// Move Office reference into array if it doesn't already exist
 		if(customerAddressID.indexOf(customersProcessed.data[i][4]) < 0){
 			customerAddressID.push(customersProcessed.data[i][4]);
 		}
 	}
-	console.log("Customers Entered Correctly");
 }
 
 function outputInvoices(){
 	// Compile each invoice line into a variable
 	let invoiceLine = "";
 	for(let i = 0; i < invoicesProcessed.data.length; i++){
+		// Join array with original delimiter for readability
 		invoiceLine = invoicesProcessed.data[i].join("|");
-		
 		// Create new elements in the DOM to output all invoice lines
 		let node = document.createElement("li");
 		node.innerHTML = invoiceLine;
@@ -102,6 +114,7 @@ function outputInvoices(){
 	
 }
 
+// Validate invoice data
 function outputErrors(){
 	hasCostCode();
 	doesClientExist();
@@ -112,7 +125,7 @@ function outputErrors(){
 	taxValue();
 }
 
-// Does Client Exist in Oracle
+// Does Client Exist
 // Invoice Field 2 [1]
 function doesClientExist(){
 	for(let i = 0; i < invoicesProcessed.data.length; i++){
@@ -125,7 +138,7 @@ function doesClientExist(){
 	}
 }
 
-// Does Office Exist in Oracle
+// Does Office Exist
 // Invoice Field 4 [3]
 function doesOfficeExist(){
 	for(let i = 0; i < invoicesProcessed.data.length; i++){
@@ -147,8 +160,7 @@ function hasCostCode(){
 			let node = document.createElement("li");
 			node.innerHTML = "Invoice " + invoicesProcessed.data[i][4] + " in file " + fileInput.files[0].name + " - No Cost Code at line " + (i + 1) + "<br><em>Invoice Line: " + invoicesProcessed.data[i].join("|") + "</em>"; 
 			invoiceErrors.appendChild(node);
-			
-			// Add temporary fields to the array to prevent further errors
+			// Add 3 temporary fields to the array to prevent further errors
 			for(let y = 0; y < 3; y++){
 				invoicesProcessed.data[i].splice(6, 0, "");
 			}
@@ -156,7 +168,7 @@ function hasCostCode(){
 	}	
 }
 
-// Has Invoice Prefix
+// Has Invoice Prefix (Invoice should be prefixed with 5 characters)
 // Invoice Field 5 [4]
 function hasInvoicePrefix(){
 	for(let i = 0; i < invoicesProcessed.data.length; i++){
@@ -174,7 +186,7 @@ function hasInvoicePrefix(){
 // Description Length
 // Invoice Field [11]
 function descriptionLength(){
-	// Description is greater than 180 chars
+	// Description is greater than 180 characters
 	for(let i=0; i < invoicesProcessed.data.length; i++){
 		if(invoicesProcessed.data[i][11].length > 180){
 			// Create new elements in the DOM to output errors
@@ -182,14 +194,13 @@ function descriptionLength(){
 			node.innerHTML = "Invoice " + invoicesProcessed.data[i][4] + " in file " + fileInput.files[0].name + " - Description too long at line " + (i + 1) + "<br><em>Invoice Line: " + invoicesProcessed.data[i].join("|") + "</em>"; 
 			invoiceErrors.appendChild(node);
 		}
-		
+		// Description is blank
 		if(invoicesProcessed.data[i][11] == ""){
 			// Create new elements in the DOM to output errors
 			let node = document.createElement("li");
 			node.innerHTML = "Invoice " + invoicesProcessed.data[i][4] + " in file " + fileInput.files[0].name + " - Description null at line " + (i + 1) + "<br><em>Invoice Line: " + invoicesProcessed.data[i].join("|") + "</em>"; 
 			invoiceErrors.appendChild(node);
 		}
-
 	}
 }
 
@@ -220,4 +231,3 @@ function taxValue(){
 		}
 	}
 }
-
